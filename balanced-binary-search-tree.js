3182c8ee86c7df762375cc31916973e500ae595b
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-28 07:58:15 +0800

    feat: Add method that builds the tree

--- balanced-binary-search-tree.js
+++ balanced-binary-search-tree.js
@@ -5,3 +5,25 @@ class Node {
         this.right = right;
     }
 }
+
+class Tree {
+    constructor(array) {
+        this.root = this.buildTree(array);
+    }
+    buildTree(array) {
+        // Base case.
+        if (array.length === 0) return null;
+
+        // Get the midpoint.
+        let midpoint = parseInt(array.length / 2);
+
+        // Create Node.
+        let root = new Node(array[midpoint]);
+
+        // Set the left and right subtree.
+        root.left = this.buildTree(array.slice(0, midpoint));
+        root.right = this.buildTree(array.slice(midpoint + 1));
+
+        return root;
+    }
+}
